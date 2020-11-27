c5efeeecae9849c440967d9ccea9e0091e13e7f8
Yassir RAMDANI <yassir.ramdani@epita.fr>
2020-11-27 04:16:30 +0100

    updated tests

--- Tests/SwiLexTests/CalculatorTokensTests.swift
+++ Tests/SwiLexTests/CalculatorTokensTests.swift
@@ -9,6 +9,9 @@ final class CalculatorTokensTests: XCTestCase {
         case int = "[0-9]+"
         case lParenthesis = #"\("#
         case rParenthesis = #"\)"#
+
+        case eof
+        case none
     }
 
     func testIntSum() throws {
@@ -16,9 +19,10 @@ final class CalculatorTokensTests: XCTestCase {
         let tokens = try lexer.lex(input: "1 + 1")
         AssertEqualTokens(tokens1: tokens,
                           tokens2: [
-                              Token<CalculatorTokens>(type: .int, value: "1"),
-                              Token<CalculatorTokens>(type: .op, value: "+"),
-                              Token<CalculatorTokens>(type: .int, value: "1"),
+                            Token<CalculatorTokens>(type: .int, value: "1"),
+                            Token<CalculatorTokens>(type: .op, value: "+"),
+                            Token<CalculatorTokens>(type: .int, value: "1"),
+                            Token<CalculatorTokens>(type: .eof, value: ""),
                           ])
     }
 
@@ -27,9 +31,10 @@ final class CalculatorTokensTests: XCTestCase {
         let tokens = try lexer.lex(input: "1.2   +      1.004")
         AssertEqualTokens(tokens1: tokens,
                           tokens2: [
-                              Token<CalculatorTokens>(type: .float, value: "1.2"),
-                              Token<CalculatorTokens>(type: .op, value: "+"),
-                              Token<CalculatorTokens>(type: .float, value: "1.004"),
+                            Token<CalculatorTokens>(type: .float, value: "1.2"),
+                            Token<CalculatorTokens>(type: .op, value: "+"),
+                            Token<CalculatorTokens>(type: .float, value: "1.004"),
+                            Token<CalculatorTokens>(type: .eof, value: ""),
                           ])
     }
 
@@ -38,9 +43,10 @@ final class CalculatorTokensTests: XCTestCase {
         let tokens = try lexer.lex(input: "   1332.4322  +       1   ")
         AssertEqualTokens(tokens1: tokens,
                           tokens2: [
-                              Token<CalculatorTokens>(type: .float, value: "1332.4322"),
-                              Token<CalculatorTokens>(type: .op, value: "+"),
-                              Token<CalculatorTokens>(type: .int, value: "1"),
+                            Token<CalculatorTokens>(type: .float, value: "1332.4322"),
+                            Token<CalculatorTokens>(type: .op, value: "+"),
+                            Token<CalculatorTokens>(type: .int, value: "1"),
+                            Token<CalculatorTokens>(type: .eof, value: ""),
                           ])
     }
 
@@ -50,27 +56,28 @@ final class CalculatorTokensTests: XCTestCase {
         print(tokens)
         AssertEqualTokens(tokens1: tokens,
                           tokens2: [
-                              Token<CalculatorTokens>(type: .lParenthesis, value: "("),
-                              Token<CalculatorTokens>(type: .float, value: "1332.4322"),
-                              Token<CalculatorTokens>(type: .op, value: "+"),
-                              Token<CalculatorTokens>(type: .int, value: "1"),
-                              Token<CalculatorTokens>(type: .rParenthesis, value: ")"),
-                              Token<CalculatorTokens>(type: .op, value: "*"),
-                              Token<CalculatorTokens>(type: .int, value: "2"),
-                              Token<CalculatorTokens>(type: .op, value: "/"),
-                              Token<CalculatorTokens>(type: .float, value: "44.44"),
-                              Token<CalculatorTokens>(type: .op, value: "+"),
-                              Token<CalculatorTokens>(type: .lParenthesis, value: "("),
-                              Token<CalculatorTokens>(type: .lParenthesis, value: "("),
-                              Token<CalculatorTokens>(type: .float, value: "2.3"),
-                              Token<CalculatorTokens>(type: .op, value: "-"),
-                              Token<CalculatorTokens>(type: .int, value: "2"),
-                              Token<CalculatorTokens>(type: .rParenthesis, value: ")"),
-                              Token<CalculatorTokens>(type: .op, value: "*"),
-                              Token<CalculatorTokens>(type: .int, value: "4"),
-                              Token<CalculatorTokens>(type: .rParenthesis, value: ")"),
-                              Token<CalculatorTokens>(type: .op, value: "/"),
-                              Token<CalculatorTokens>(type: .float, value: "0.3"),
+                            Token<CalculatorTokens>(type: .lParenthesis, value: "("),
+                            Token<CalculatorTokens>(type: .float, value: "1332.4322"),
+                            Token<CalculatorTokens>(type: .op, value: "+"),
+                            Token<CalculatorTokens>(type: .int, value: "1"),
+                            Token<CalculatorTokens>(type: .rParenthesis, value: ")"),
+                            Token<CalculatorTokens>(type: .op, value: "*"),
+                            Token<CalculatorTokens>(type: .int, value: "2"),
+                            Token<CalculatorTokens>(type: .op, value: "/"),
+                            Token<CalculatorTokens>(type: .float, value: "44.44"),
+                            Token<CalculatorTokens>(type: .op, value: "+"),
+                            Token<CalculatorTokens>(type: .lParenthesis, value: "("),
+                            Token<CalculatorTokens>(type: .lParenthesis, value: "("),
+                            Token<CalculatorTokens>(type: .float, value: "2.3"),
+                            Token<CalculatorTokens>(type: .op, value: "-"),
+                            Token<CalculatorTokens>(type: .int, value: "2"),
+                            Token<CalculatorTokens>(type: .rParenthesis, value: ")"),
+                            Token<CalculatorTokens>(type: .op, value: "*"),
+                            Token<CalculatorTokens>(type: .int, value: "4"),
+                            Token<CalculatorTokens>(type: .rParenthesis, value: ")"),
+                            Token<CalculatorTokens>(type: .op, value: "/"),
+                            Token<CalculatorTokens>(type: .float, value: "0.3"),
+                            Token<CalculatorTokens>(type: .eof, value: ""),
                           ])
     }
 
